f0e7e20dda6f9c3cdb14f5a369b768ce7c99d962
nayeemdev <nayeem9812@gmail.com>
2023-04-02 03:24:27 +0600

    fix: handle exception

--- app/Http/Controllers/ApiIntegrationController.php
+++ app/Http/Controllers/ApiIntegrationController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\ApiIntegrationRequest;
 use App\Services\SubscriberService;
+use GuzzleHttp\Exception\GuzzleException;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Log;
 use Illuminate\View\View;
 
 class ApiIntegrationController extends Controller
@@ -28,10 +30,14 @@ class ApiIntegrationController extends Controller
      */
     public function apiValidate(ApiIntegrationRequest $request): RedirectResponse
     {
-        $subscriberService = new SubscriberService($request->api_key);
+        try {
+            $subscriberService = new SubscriberService($request->api_key);
 
-        if ($subscriberService->validateApiKey()) {
-            return back()->with('success', 'Key is valid and ready to use. You can now subscribe to the newsletter.');
+            if ($subscriberService->validateApiKey()) {
+                return back()->with('success', 'Key is valid and ready to use. You can subscribe to the newsletter.');
+            }
+        } catch (\Exception|GuzzleException $e) {
+            Log::error($e->getMessage());
         }
 
         return back()->with('error', 'API key is not valid.');
