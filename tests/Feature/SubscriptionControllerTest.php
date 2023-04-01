175fc8eb1e61877602bec9d6768be3b64ae8795a
nayeemdev <nayeem9812@gmail.com>
2023-04-02 03:42:08 +0600

    test: added feature test subscribers

---
+++ tests/Feature/SubscriptionControllerTest.php
@@ -0,0 +1,215 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\ApiKey;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class SubscriptionControllerTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    protected $connection = 'mysql';
+
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function test_root_url_should_redirect()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(302);
+    }
+
+    public function test_subscription_wont_work_without_api_key()
+    {
+        $response = $this->get('/subscribers');
+        $response->assertStatus(302);
+        $response->assertSessionHas('error');
+        $this->assertEquals('API key is not set.', $response->getSession()->get('error'));
+    }
+
+    public function test_subscription_will_work_with_api_key()
+    {
+        ApiKey::updateOrCreate(
+            ['name' => 'mailerlite'],
+            ['key' => 'test']
+        );
+        $response = $this->get('/subscribers');
+        $response->assertStatus(200);
+    }
+
+    public function test_user_can_see_api_integration_page()
+    {
+        $response = $this->get(route('integration.show'));
+
+        $response->assertStatus(200);
+        $response->assertSee('API Key');
+        $response->assertViewIs('api-integration.validate');
+    }
+
+    public function test_user_can_see_api_key_in_api_integration_page()
+    {
+        $this->setAPiKey();
+        $response = $this->get(route('integration.show'));
+
+        $this->assertDatabaseHas('api_keys', [
+            'name' => 'mailerlite',
+            'key' => $this->apiKey,
+        ]);
+
+        $response->assertStatus(200);
+        $response->assertSee($this->apiKey);
+    }
+
+    public function test_user_can_validate_save_api_key()
+    {
+        $response = $this->post(route('integration.validate'), [
+            'api_key' => $this->apiKey,
+        ]);
+
+        $this->assertDatabaseHas('api_keys', [
+            'name' => 'mailerlite',
+            'key' => $this->apiKey,
+        ]);
+
+        $response->assertStatus(302);
+        $response->assertSessionHas('success');
+        $this->assertEquals('Key is valid and ready to use. You can subscribe to the newsletter.', $response->getSession()->get('success'));
+    }
+
+    public function test_user_can_not_validate_save_api_key_with_invalid_key()
+    {
+        $response = $this->post(route('integration.validate'), [
+            'api_key' => 'invalid_key',
+        ]);
+
+        $this->assertDatabaseMissing('api_keys', [
+            'name' => 'mailerlite',
+            'key' => 'invalid_key',
+        ]);
+
+        $response->assertStatus(302);
+        $response->assertSessionHas('error');
+        $this->assertEquals('API key is not valid.', $response->getSession()->get('error'));
+    }
+
+    public function test_user_can_see_subscribers_page()
+    {
+        $this->setAPiKey();
+        $response = $this->get(route('subscribers.index'));
+
+        $response->assertStatus(200);
+        $response->assertSee('Subscribers');
+        $response->assertViewIs('subscribers.index');
+    }
+
+    public function test_user_can_subscribe_page_with_form()
+    {
+        $this->setAPiKey();
+        $response = $this->get(route('subscribers.create'));
+
+        $response->assertStatus(200);
+        $response->assertSee('Subscribe');
+        $response->assertViewIs('subscribers.create');
+    }
+
+    public function test_user_can_subscribe_with_valid_data()
+    {
+        $this->setAPiKey();
+        $data = [
+            'email' => 'test@test-test.test',
+            'name' => 'Test Test',
+            'country' => 'Bangladesh',
+        ];
+
+        $response = $this->post(route('subscribers.store'), $data);
+
+        $response->assertStatus(302);
+        $response->assertSessionHas('success');
+        $this->assertDatabaseHas('subscribers', [
+            'email' => $data['email']
+        ]);
+        $this->assertEquals('Subscriber created successfully.', $response->getSession()->get('success'));
+    }
+
+    public function test_user_can_not_subscribe_with_invalid_data()
+    {
+        $this->setAPiKey();
+        $data = [
+            'email' => 'test@test-test.test',
+            'name' => 'Test Test',
+            'country' => 'test',
+        ];
+
+        $response = $this->post(route('subscribers.store'), $data);
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors(['country']);
+        $response->assertSessionHasErrors(['country' => 'The country is not valid.']);
+    }
+
+    public function test_user_can_not_subscribe_with_invalid_email()
+    {
+        $this->setAPiKey();
+        $data = [
+            'email' => 'test',
+            'name' => 'Test Test',
+            'country' => 'Bangladesh',
+        ];
+
+        $response = $this->post(route('subscribers.store'), $data);
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors(['email']);
+        $response->assertSessionHasErrors(['email' => 'The email must be a valid email address.']);
+    }
+
+    public function test_user_can_update_subscriber()
+    {
+        $this->setAPiKey();
+        $data = [
+            'email' => 'test@test-test.test',
+            'name' => 'Test Test',
+            'country' => 'Bangladesh',
+        ];
+
+        $this->post(route('subscribers.store'), $data);
+
+        $data['name'] = 'Test Test 2';
+
+        $subscriber_id = \App\Models\Subscriber::first()->subscriber_id;
+
+        $response = $this->put(route('subscribers.update', $subscriber_id), $data);
+
+        $response->assertStatus(302);
+        $response->assertSessionHas('success');
+        $this->assertEquals('Subscriber updated successfully.', $response->getSession()->get('success'));
+    }
+
+    public function test_user_can_delete_subscriber()
+    {
+        $this->setAPiKey();
+        $data = [
+            'email' => 'test@test-test.test',
+            'name' => 'Test Test',
+            'country' => 'Bangladesh',
+        ];
+        $this->post(route('subscribers.store'), $data);
+
+        $subscriber_id = \App\Models\Subscriber::first()->subscriber_id;
+
+        $response = $this->delete(route('subscribers.destroy', $subscriber_id));
+
+        $response->assertStatus(302);
+        $response->assertSessionHas('success');
+        $this->assertEquals('Subscriber deleted successfully.', $response->getSession()->get('success'));
+        $this->assertDatabaseMissing('subscribers', [
+            'email' => $data['email']
+        ]);
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,7 @@
 
 namespace Tests;
 
+use App\Models\ApiKey;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
@@ -11,4 +12,12 @@ abstract class TestCase extends BaseTestCase
     // this is not a good way to use apiKey like this, I am using this for testing purpose only
     // I will remove this from server after finishing the test
     public $apiKey = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI0IiwianRpIjoiMzU4Mzc3N2NlMWM3YzNkMmY4OGI0MjI5NTU2YWYyMGExM2UxYjJkNWFiM2EwNjMyZmMyMjljZTc2NGZmNGViMTVmYWE4ZDQyZDU3MzM4NWEiLCJpYXQiOjE2ODAwMzgwNzYuMjcwOTg0LCJuYmYiOjE2ODAwMzgwNzYuMjcwOTg4LCJleHAiOjQ4MzU3MTE2NzYuMjY2NDA5LCJzdWIiOiI0MTEyNDIiLCJzY29wZXMiOltdfQ.HvHK9PCD_CYqUOmZNlfG0ZsFdSPkSTJKDkGhAZZXQicX7yM5gQLDBf4gKZIanyrutm_SD8AXV5PjwLy9MUzkdy1-F8V2l77mvOlwvvRMVfv5klrh7_uLrg_sBuCrE6AiubpTbEhrIZsnJshUjVaB4UhXQOOxTueK4DL9Na2HY03W6F3NP825CvlgBD8jmNQFh9VLMl0074Fo_ainQA4M75wyAhKZLzlearUPN7WUxLtISLiRlMgoOUeTy1_8U0oeDaD5-zSwNtBrrBrnC8xDVnRJkCh-oLZVRgNH_y4rYwegnN1ixtQk9ux02AEfhzl7AK75sPbMZUqvTnNPduT_cGHd-Qxa6cjMxx5-J7bHJhdugaC0yMfSa0VQ1r1pJ7veFlHKg3GjbQvZIbcNZKxbCSgnvxTU3Rg_qMVJGGh8-bpqOUBauhlmuyCTIrfjlbM1oHICnYsZTEYu54dyKuW2hi9uaxRzG1k2KS21cAC-Z7aQfjoNApw1wTM5Z3r4RqgnW82pN5PmDy3OosKtHKZfeHugYs4NsmmzYNIW8DHqZY7LbQn2v7AOGwWe-0pyajM0W6crrfspLbRXgpCqcF-wKNbAayeK1N8xS5gNVnsleT58aaxb1-zFX3a72kbPmz4k-rJ9bxVHIC3sC0Opkw8TqEk8ssSNJIbfTTMtFlr6tN0';
+
+    public function setAPiKey()
+    {
+        ApiKey::updateOrCreate(
+            ['name' => 'mailerlite'],
+            ['key' => $this->apiKey]
+        );
+    }
 }
